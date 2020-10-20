d93c28b99d06918d74fef54dcec29c5dc0b3d494
GergesBernaba1 <gergesbernaba2@gmail.com>
2020-10-20 19:56:14 +0200

    angular-training-task5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './content/main/main.component';
+import { ListComponent } from './content/list/list.component';
+import { ItemComponent } from './content/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
