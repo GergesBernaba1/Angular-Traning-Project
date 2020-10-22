4de5ad998df7060978da6ed4ea4eb3f994efb00c
GergesBernaba1 <gergesbernaba2@gmail.com>
2020-10-22 12:37:23 +0200

    Routes added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-internship'`, () => {
+  it(`should have as title 'Angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-internship');
+    expect(app.title).toEqual('Angular-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-internship app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular-project app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { ItemComponent } from './content/item/item.component';
 import { MainComponent } from './content/main/main.component';
 import { ListComponent } from './content/list/list.component';
-import { ItemComponent } from './content/item/item.component';
+import { ContentModule } from './content/content.module';
+import {RoutesRoutingModule} from './content/routes/routes-routing.module';
+import {RouterModule, Routes} from '@angular/router';
+import { start } from 'repl';
+
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'item', component: ItemComponent },
+  { path: 'main', component: MainComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppNavComponent,
+    ItemComponent,
     MainComponent,
     ListComponent,
-    ItemComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    ContentModule,
+    RoutesRoutingModule,
+    RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
